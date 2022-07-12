112034d12d56b11a451c45204419fe2f4d8292a5
darkCavalier11 <57953014+darkCavalier11@users.noreply.github.com>
2022-07-12 12:43:56 +0530

    added sideactivity

--- example/android/app/src/main/kotlin/com/example/flutter_thermal_printer_example/SideActivity.kt
+++ example/android/app/src/main/kotlin/com/example/flutter_thermal_printer_example/SideActivity.kt
@@ -1,17 +1,15 @@
 package com.example.flutter_thermal_printer_example
 
 import android.os.Bundle
-import android.os.Environment
 import android.os.PersistableBundle
 import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugins.GeneratedPluginRegistrant
+import java.lang.Exception
 
-class MainActivity: FlutterActivity() {
+class SideActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
-        Log.d("Cutu", "HelloWorld")
+        Log.d("ComeOn!", "HelloW@orld")
+        throw Exception("Blah!")
     }
 }
