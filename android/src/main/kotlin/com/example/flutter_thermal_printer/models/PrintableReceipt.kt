94bbf04d2ac6cf8dfc0d365c07effa2a46072177
darkCavalier11 <sumit.kumar@changepay.in>
2025-03-15 16:27:52 +0530

    added padding for qr code

--- android/src/main/kotlin/com/example/flutter_thermal_printer/models/PrintableReceipt.kt
+++ android/src/main/kotlin/com/example/flutter_thermal_printer/models/PrintableReceipt.kt
@@ -69,8 +69,16 @@ class PrintableReceipt(
         if (qrCodeText != null) {
             list.add(DataForSendToPrinterPos58.initializePrinter())
             list.add(DataForSendToPrinterPos58.selectAlignment(1))
+            list.add(DataForSendToPrinterPos58.printAndFeedLine())
+            list.add(DataForSendToPrinterPos58.printAndFeedLine())
+            list.add(DataForSendToPrinterPos58.printAndFeedLine())
+            list.add(DataForSendToPrinterPos58.printAndFeedLine())
             list.add(qrCodeDataToByteArray(qrCodeText, 250)!!)
             list.add(DataForSendToPrinterPos58.printAndFeedLine())
+            list.add(DataForSendToPrinterPos58.printAndFeedLine())
+            list.add(DataForSendToPrinterPos58.printAndFeedLine())
+            list.add(DataForSendToPrinterPos58.printAndFeedLine())
+
         }
 
         list.add("--------------------------------".encodeToByteArray())
